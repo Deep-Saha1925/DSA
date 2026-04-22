c63de107128d65245324f7db6da17657ef059b6e
Deep Saha <dip23447@gmail.com>
2026-04-22 13:02:29 +0530

    Modified method

--- Infix_Postfix.py
+++ Infix_Postfix.py
@@ -19,51 +19,134 @@ class Stack:
         return self.stack[-1]
 
 
-def precedence(e):
-    if e == '$' or e == '^':
+def precedence(op):
+    if op in ('^', '$'):
         return 3
-    if e == '%' or e == '/' or e == '*':
+    if op in ('*', '/', '%'):
         return 2
-    if e == '+' or e == '-':
+    if op in ('+', '-'):
         return 1
     return 0
 
+# s = Stack()
+# exp = ""
 
-s = Stack()
-exp = ""
+# # starting bracket
+# s.push('(')
 
-# starting bracket
-s.push('(')
+# l = []
+# while True:
+#     val = input("Enter character: ").lower()
+#     if val == 'x':
+#         break
+#     l.append(val)
 
-l = []
+# # Add closing parenthesis
+# l.append(')')
+
+# # Your input
+# # l = ['A', '+', 'B', '(', 'C', '*', 'D', '+', 'F', ')', ')']
+
+# for val in l:
+#     if val.isalpha():
+#         exp += val
+
+#     elif val == '(':
+#         s.push(val)
+
+#     elif val == ')':
+#         while s.peek() != '(':
+#             exp += s.pop()
+#         s.pop()  # remove '('
+
+#     else:
+#         # keep popping until condition satisfied
+#         while (not s.is_empty()) and precedence(val) <= precedence(s.peek()):
+#             exp += s.pop()
+#         s.push(val)
+
+# print("Postfix:", exp)
+
+
+# ====================================================================================================================================
+
+
+
+# EVALUATE USING VALUE
+
+# 🔹 TAKE INPUT CHARACTER BY CHARACTER
+print("Enter expression character by character (press x to stop):")
+
+tokens = []
+num = ""
+
 while True:
-    val = input("Enter character: ").lower()
-    if val == 'x':
+    ch = input("Enter: ")
+
+    if ch == 'x':
         break
-    l.append(val)
 
-# Add closing parenthesis
-l.append(')')
+    if ch.isdigit():
+        num += ch
+    else:
+        if num:
+            tokens.append(num)
+            num = ""
+        tokens.append(ch)
 
-# Your input
-# l = ['A', '+', 'B', '(', 'C', '*', 'D', '+', 'F', ')', ')']
+# last number
+if num:
+    tokens.append(num)
 
-for val in l:
-    if val.isalpha():
-        exp += val
 
-    elif val == '(':
-        s.push(val)
+# INFIX → POSTFIX
+s = Stack()
+postfix = []
 
-    elif val == ')':
+s.push('(')
+tokens.append(')')
+
+for token in tokens:
+    if token.isdigit():
+        postfix.append(token)
+
+    elif token == '(':
+        s.push(token)
+
+    elif token == ')':
         while s.peek() != '(':
-            exp += s.pop()
-        s.pop()  # remove '('
+            postfix.append(s.pop())
+        s.pop()
 
     else:
-        # keep popping until condition satisfied
-        while (not s.is_empty()) and precedence(val) <= precedence(s.peek()):
-            exp += s.pop()
-        s.push(val)
+        while precedence(token) <= precedence(s.peek()):
+            postfix.append(s.pop())
+        s.push(token)
 
-print("Postfix:", exp)
+print("Postfix:", " ".join(postfix))
+
+
+# 🔹 EVALUATE
+s2 = Stack()
+
+for token in postfix:
+    if token.isdigit():
+        s2.push(int(token))
+    else:
+        b = s2.pop()
+        a = s2.pop()
+
+        if token == '+':
+            s2.push(a + b)
+        elif token == '-':
+            s2.push(a - b)
+        elif token == '*':
+            s2.push(a * b)
+        elif token == '/':
+            s2.push(a / b)
+        elif token == '%':
+            s2.push(a % b)
+        elif token == '^':
+            s2.push(a ** b)
+
+print("Result:", s2.pop())
