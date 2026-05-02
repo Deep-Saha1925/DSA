6813034fb47a75ecab4b3684521451ecba4c1e35
Deep Saha <dip23447@gmail.com>
2026-05-02 23:50:49 +0530

    Car Pooling

---
+++ CarPooling_1094.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] stops = new int[1001]; // constraints: location <= 1000
+        
+        // mark changes
+        for (int[] trip : trips) {
+            int passengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+            
+            stops[from] += passengers;  // pick up
+            stops[to] -= passengers;    // drop
+        }
+        
+        int current = 0;
+        
+        // check capacity
+        for (int i = 0; i < 1001; i++) {
+            current += stops[i];
+            if (current > capacity) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
