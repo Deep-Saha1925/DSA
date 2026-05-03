1642429759a6d0deaaa0d45c9d14b5cff102a35e
Deep Saha <dip23447@gmail.com>
2026-05-03 21:36:13 +0530

    Edit Distance modified using DP

--- EditDistance_72.java
+++ EditDistance_72.java
@@ -1,22 +1,29 @@
+import java.util.Arrays;
 class Solution {
 
-    private int f(String a, String b, int i, int j){
+    private int f(String a, String b, int i, int j, int[][] dp){
         if(i < 0) return j+1;
         if(j < 0) return i+1;
 
-        if(a.charAt(i) == b.charAt(j))  return f(a, b, i-1, j-1);
+        if(dp[i][j] != -1) return dp[i][j];
 
-        return 1 + Math.min(
-            f(a, b, i, j-1),    // insert
+        if(a.charAt(i) == b.charAt(j))  return dp[i][j] = f(a, b, i-1, j-1, dp);
+
+        return dp[i][j] =  1 + Math.min(
+            f(a, b, i, j-1, dp),    // insert
             Math.min(
-                f(a, b, i-1, j), //remove
-                f(a, b, i-1, j-1)    //replace
+                f(a, b, i-1, j, dp), //remove
+                f(a, b, i-1, j-1, dp)    //replace
             )
         );
     }
 
     public int minDistance(String word1, String word2) {
         int n = word1.length(), m = word2.length();
-        return f(word1, word2, n, m);
+        int[][] dp = new int[n][m];
+        for(int i=0; i<dp.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return f(word1, word2, n-1, m-1, dp);
     }
 }
