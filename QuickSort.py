d9e436f82ead59cce1bb0e393f8293672bc78f86
Deep Saha <dip23447@gmail.com>
2026-05-05 15:13:04 +0530

    Modified Quick sort.

--- QuickSort.py
+++ QuickSort.py
@@ -29,9 +29,44 @@ def quickSort(l, i, j):
         quickSort(l, i, p-1)
         quickSort(l, p+1, j)
 
+count=0
+def quick(a, l, r):
+    p = l
+    left = l+1
+    right = r
+    
+    while True:
+        while a[p] <= a[right] and right >= left:
+            right -= 1
+        
+        if a[p] > a[right]:
+            a[p], a[right] = a[right], a[p]
+            p = right
+            right -= 1
+            
+        if right < left:
+            return p
+        
+        while a[p] >= a[left] and right >= left:
+            left += 1
+            
+        if a[p] < a[left]:
+            a[p], a[left] = a[left], a[p]
+            p = left
+            left += 1
+            
+        if right < left:
+            return p            
 
+def quickSort2(l, i, j):
+    if i < j:
+        p = quick(l, i, j)
+        quickSort2(l, i, p-1)
+        quickSort2(l, p+1, j)
+        count+=1
 
-
-l = [0, 17, -3, 90, -14, 0, 1, 100]
-quickSort(l, 0, len(l)-1)
+l = [9,4,12,7,1]
 print(l)
+quickSort2(l, 0, len(l)-1)
+print(l)
+print(count)
