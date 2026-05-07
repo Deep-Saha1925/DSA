b7a332de25f2e7299c6e2f7d994a40de145d2f1b
Deep Saha <dip23447@gmail.com>
2026-05-07 22:32:55 +0530

    Insert at position DLL

--- py/DoublyLL.py
+++ py/DoublyLL.py
@@ -27,7 +27,33 @@ class DoublyLinkedList:
         new_node.next = self.head
         self.head = new_node
     
-    
+    def insert_at_position(self, data, position):
+        new_node = Node(data)
+        if position == 0:
+            self.insert_at_beginning(data)
+            return
+        
+        if position < 0:
+            print("Position must be a non-negative integer.")
+            return
+        
+        current = self.head
+        current_position = 0
+        while current != None and current_position < position - 1:
+            current = current.next
+            current_position += 1
+            
+        if current is None:
+            print("Position out of bounds.")
+            return
+        
+        new_node.next = current.next
+        new_node.prev = current
+        
+        if current.next is not None:
+            current.next.prev = new_node
+            
+        current.next = new_node
         
     def traverse(self):
         current = self.head
