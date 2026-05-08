a851287dc2cd21725c6bb202864c98bde7391609
Deep Saha <dip23447@gmail.com>
2026-05-08 13:23:51 +0530

    Modified 64

--- MinimumSumPath_64.java
+++ MinimumSumPath_64.java
@@ -2,7 +2,32 @@ class Solution {
 
     public int minPathSum(int[][] grid) {
 
-        return solve(grid, 0, 0);
+        //return solve(grid, 0, 0);
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // first row
+        for(int i=1; i<n; i++){
+            grid[0][i] += grid[0][i-1];
+        }
+
+        // first column
+        for(int i=1; i<m; i++){
+            grid[i][0] += grid[i-1][0];
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                grid[i][j] += Math.min(
+                    grid[i-1][j],
+                    grid[i][j-1]
+                );
+            }
+        }
+
+        return grid[m-1][n-1];
+
     }
 
     int solve(int[][] grid, int i, int j) {
