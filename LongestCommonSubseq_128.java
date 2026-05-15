ee08c3e27d7c217085a38df7ea0279c167ad51ba
Deep-Saha1925 <dip23447@gmail.com>
2026-05-15 15:49:18 +0530

    Longest Common Subsequence

---
+++ LongestCommonSubseq_128.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+class LongestCommonSubseq_128 {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int n: nums){
+            set.add(n);
+        }
+
+        int longest = 0;
+
+        for(int n: set){
+            if(!set.contains(n-1)){
+                int curr = n;
+                int count = 1;
+
+                while(set.contains(curr+1)){
+                    curr++;
+                    count++;
+                }
+
+                longest = Math.max(longest, count);
+            }
+        }
+
+        return longest;
+    }
+}
