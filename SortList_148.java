6a782d94522ca6f050c758db05716351a03c70bc
Deep-Saha1925 <dip23447@gmail.com>
2026-05-19 23:32:13 +0530

    Modified

--- SortList_148.java
+++ SortList_148.java
@@ -14,6 +14,17 @@
 
 class Solution {
 
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+    }
+
     public ListNode sortList(ListNode head) {
 
         // Base case
