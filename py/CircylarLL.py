f7ba1916aaac8b9ea77802a3162633aa453268ea
Deep-Saha1925 <dip23447@gmail.com>
2026-05-20 19:45:32 +0530

    Delete from circular LL

--- py/CircylarLL.py
+++ py/CircylarLL.py
@@ -19,6 +19,34 @@ class CircularLL:
         temp.next = new_node
         new_node.next = self.head
 
+    def delete(self, key):
+        if not self.head:
+            print("List is empty")
+            return
+        temp = self.head
+        prev = None
+        while True:
+            if temp.data == key:
+                if prev:
+                    prev.next = temp.next
+                else:
+                    # Deleting the head node
+                    if temp.next == self.head:  # Only one node in the list
+                        self.head = None
+                    else:
+                        # Find the last node to update its next pointer
+                        last_node = self.head
+                        while last_node.next != self.head:
+                            last_node = last_node.next
+                        last_node.next = temp.next
+                        self.head = temp.next
+                return
+            prev = temp
+            temp = temp.next
+            if temp == self.head:
+                break
+        print(f"Node with data {key} not found")
+
     def display(self):
         if not self.head:
             print("List is empty")
