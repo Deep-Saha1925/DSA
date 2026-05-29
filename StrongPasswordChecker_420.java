96717399c625a1b3257340b41cc1b724d0de1b20
Deep-Saha1925 <dip23447@gmail.com>
2026-05-29 21:41:32 +0530

    Modified password checker

--- StrongPasswordChecker_420.java
+++ StrongPasswordChecker_420.java
@@ -13,7 +13,83 @@ class Solution {
 
         int replace = 0;
 
-        for(int i = 2; i < s.length(); i++) {
+        for(int i = 2; i < s.length(); i++) {class Solution {
+    public int strongPasswordChecker(String password) {
+
+        int n = password.length();
+
+        int lower = 1, upper = 1, digit = 1;
+
+        for (char c : password.toCharArray()) {
+            if (Character.isLowerCase(c)) lower = 0;
+            else if (Character.isUpperCase(c)) upper = 0;
+            else if (Character.isDigit(c)) digit = 0;
+        }
+
+        int missing = lower + upper + digit;
+
+        int replace = 0;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n;) {
+            int j = i;
+            while (j < n && password.charAt(j) == password.charAt(i))
+                j++;
+
+            arr[i] = j - i;
+
+            if (arr[i] >= 3)
+                replace += arr[i] / 3;
+
+            i = j;
+        }
+
+        if (n < 6)
+            return Math.max(missing, 6 - n);
+
+        if (n <= 20)
+            return Math.max(missing, replace);
+
+        int delete = n - 20;
+        int remainDelete = delete;
+
+        // First: groups where len % 3 == 0
+        for (int i = 0; i < n && remainDelete > 0; i++) {
+            if (arr[i] >= 3 && arr[i] % 3 == 0) {
+                arr[i]--;
+                remainDelete--;
+                replace--;
+            }
+        }
+
+        // Second: groups where len % 3 == 1
+        for (int i = 0; i < n && remainDelete > 1; i++) {
+            if (arr[i] >= 3 && arr[i] % 3 == 1) {
+                arr[i] -= 2;
+                remainDelete -= 2;
+                replace--;
+            }
+        }
+
+        // Third: groups where len % 3 == 2
+        for (int i = 0; i < n && remainDelete > 0; i++) {
+
+            if (arr[i] >= 3) {
+
+                int need = arr[i] - 2;
+
+                int used = Math.min(remainDelete, need);
+
+                arr[i] -= used;
+                remainDelete -= used;
+
+                replace -= used / 3;
+            }
+        }
+
+        return delete + Math.max(missing, replace);
+    }
+}
             if(s.charAt(i) == s.charAt(i-1) &&
                s.charAt(i) == s.charAt(i-2)) {
 
