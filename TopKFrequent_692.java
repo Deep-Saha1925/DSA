2aaab86d73e43f05f9791f7c0abfa5f846402c95
Deep-Saha1925 <dip23447@gmail.com>
2026-06-12 23:13:34 +0530

    Top K Frequent words

---
+++ TopKFrequent_692.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+
+    public List<String> topKFrequent(String[] words, int k) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+
+        List<String> list = new ArrayList<>(map.keySet());
+
+        Collections.sort(list, (a, b) -> {
+
+            if(!map.get(a).equals(map.get(b))) {
+                return map.get(b) - map.get(a);
+            }
+
+            return a.compareTo(b);
+        });
+
+        return list.subList(0, k);
+    }
+}
