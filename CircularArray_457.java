81c452e31dad897137f03f9491082fa631bc7090
Deep-Saha1925 <dip23447@gmail.com>
2026-06-25 22:26:43 +0530

    Circular array loop

--- CircularArray_457.java
+++ CircularArray_457.java
@@ -1,3 +1,58 @@
-public class CircularArray_457 {
-    
+class Solution {
+
+    public boolean circularArrayLoop(int[] nums) {
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            boolean forward = nums[i] > 0;
+
+            int slow = i;
+            int fast = i;
+
+            while (true) {
+
+                slow = move(nums, slow, forward);
+
+                if (slow == -1)
+                    break;
+
+                fast = move(nums, fast, forward);
+
+                if (fast == -1)
+                    break;
+
+                fast = move(nums, fast, forward);
+
+                if (fast == -1)
+                    break;
+
+                if (slow == fast)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int move(int[] nums,
+                     int index,
+                     boolean forward) {
+
+        boolean dir = nums[index] > 0;
+
+        if (dir != forward)
+            return -1;
+
+        int n = nums.length;
+
+        int next =
+            ((index + nums[index]) % n + n) % n;
+
+        if (next == index)
+            return -1;
+
+        return next;
+    }
 }
