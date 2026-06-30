0c9c026b91f4a3f44289d8847e9b89c5d7257848
Deep-Saha1925 <dip23447@gmail.com>
2026-06-30 21:14:46 +0530

    K th largest

--- KthLargest_703.java
+++ KthLargest_703.java
@@ -1,3 +1,26 @@
-public class KthLargest_703 {
-    
+class Solution {
+
+    int count = 0;
+    int ans = 0;
+
+    public int kthLargest(TreeNode root, int k) {
+        reverseInorder(root, k);
+        return ans;
+    }
+
+    private void reverseInorder(TreeNode root, int k) {
+
+        if (root == null)
+            return;
+
+        reverseInorder(root.right, k);
+
+        count++;
+        if (count == k) {
+            ans = root.val;
+            return;
+        }
+
+        reverseInorder(root.left, k);
+    }
 }
