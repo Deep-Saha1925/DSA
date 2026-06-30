8bca0d8fe64fbd169ad411bb7d919e620dc00bea
Deep-Saha1925 <dip23447@gmail.com>
2026-06-30 21:18:07 +0530

    Min distance betwn nodes

--- MinDistNodes_783.java
+++ MinDistNodes_783.java
@@ -1,3 +1,26 @@
-public class MinDistNodes_783 {
-    
+class Solution {
+
+    TreeNode prev = null;
+    int min = Integer.MAX_VALUE;
+
+    public int minDiffInBST(TreeNode root) {
+        inorder(root);
+        return min;
+    }
+
+    private void inorder(TreeNode root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+
+        if (prev != null) {
+            min = Math.min(min, root.val - prev.val);
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
 }
