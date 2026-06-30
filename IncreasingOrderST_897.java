e5a5758a79f365ae1f62249933389f96b4b284ac
Deep-Saha1925 <dip23447@gmail.com>
2026-06-30 21:19:58 +0530

    Increasing order Search Tree

--- IncreasingOrderST_897.java
+++ IncreasingOrderST_897.java
@@ -1,3 +1,26 @@
-public class IncreasingOrderST_897 {
-    
+class Solution {
+
+    TreeNode dummy = new TreeNode(-1);
+    TreeNode curr = dummy;
+
+    public TreeNode increasingBST(TreeNode root) {
+
+        inorder(root);
+
+        return dummy.right;
+    }
+
+    private void inorder(TreeNode root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+
+        root.left = null;
+        curr.right = root;
+        curr = root;
+
+        inorder(root.right);
+    }
 }
