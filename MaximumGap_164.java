8d56646bb5000921047699531be0316b31825104
Deep-Saha1925 <dip23447@gmail.com>
2026-07-02 15:29:45 +0530

    Maximum Gap

--- MaximumGap_164.java
+++ MaximumGap_164.java
@@ -1,3 +1,18 @@
-public class MaximumGap_164 {
-    
+class Solution {
+
+    public int maximumGap(int[] nums) {
+
+        if (nums.length < 2)
+            return 0;
+
+        Arrays.sort(nums);
+
+        int maxGap = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            maxGap = Math.max(maxGap, nums[i] - nums[i - 1]);
+        }
+
+        return maxGap;
+    }
 }
