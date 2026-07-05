06e7abecacaae7c5323589e700d972dfd6c14b26
Deep-Saha1925 <dip23447@gmail.com>
2026-07-05 20:54:03 +0530

    Distant Barcodes

--- DistantBarcodes_1054.java
+++ DistantBarcodes_1054.java
@@ -2,8 +2,42 @@ class Solution {
     public int[] rearrangeBarcodes(int[] barcodes) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int n: nums){
+        for(int n: barcodes){
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for(int k: map.keySet()){
+            pq.offer(new int[]{k, map.get(k)});
+        }
+
+        int[] ans = new int[barcodes.length];
+        int idx = 0;
+
+        while(pq.size() >= 2){
+            int[] f = pq.poll();
+            int[] s = pq.poll();
+
+            ans[idx++] = f[0];
+            ans[idx++] = s[0];
+
+            f[1]--;
+            s[1]--;
+
+            if(f[1] > 0){
+                pq.offer(f);
+            }
+
+            if(s[1] > 0){
+                pq.offer(s);
+            }
+        }
+
+        if(!pq.isEmpty()){
+            ans[idx] = pq.poll()[0];
+        }
+
+        return ans;
     }
 }
