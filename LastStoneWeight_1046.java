ef10b7bdc663fd8d045a3144cb3769c35fb1dc4d
Deep-Saha1925 <dip23447@gmail.com>
2026-07-05 20:41:57 +0530

    Last Stone weight

--- LastStoneWeight_1046.java
+++ LastStoneWeight_1046.java
@@ -1,5 +1,20 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int s: stones){
+            pq.offer(s);
+        }
+
+        while(pq.size() > 1){
+            int x = pq.poll();
+            int y = pq.poll();
+
+            if(x != y){
+                pq.offer(x-y);
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.poll();
     }
 }
