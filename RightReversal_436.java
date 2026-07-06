dfe4fba43e199c7488b6137a94cde2ea5019d5c7
Deep-Saha1925 <dip23447@gmail.com>
2026-07-06 21:43:33 +0530

    Find Right Reversal

--- RightReversal_436.java
+++ RightReversal_436.java
@@ -1,5 +1,43 @@
 class Solution {
+
     public int[] findRightInterval(int[][] intervals) {
-        
+
+        int n = intervals.length;
+
+        int[][] starts = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            starts[i][0] = intervals[i][0]; // start
+            starts[i][1] = i;               // original index
+        }
+
+        Arrays.sort(starts, (a, b) -> a[0] - b[0]);
+
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            int end = intervals[i][1];
+
+            int left = 0;
+            int right = n - 1;
+            int res = -1;
+
+            while (left <= right) {
+
+                int mid = left + (right - left) / 2;
+
+                if (starts[mid][0] >= end) {
+                    res = starts[mid][1];
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans[i] = res;
+        }
+
+        return ans;
     }
 }
