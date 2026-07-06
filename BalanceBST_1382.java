e1253edde5445a70b2ec78a14c505de373ad8f4a
Deep-Saha1925 <dip23447@gmail.com>
2026-07-06 21:34:55 +0530

    Balance BST

--- BalanceBST_1382.java
+++ BalanceBST_1382.java
@@ -14,7 +14,38 @@
  * }
  */
 class Solution {
+
+    List<Integer> list = new ArrayList<>();
+
     public TreeNode balanceBST(TreeNode root) {
+
+        inorder(root);
+
+        return buildTree(0, list.size() - 1);
+    }
+
+    private void inorder(TreeNode root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+
+    private TreeNode buildTree(int left, int right) {
+
+        if (left > right)
+            return null;
+
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(list.get(mid));
+
+        root.left = buildTree(left, mid - 1);
+        root.right = buildTree(mid + 1, right);
+
         return root;
     }
 }
