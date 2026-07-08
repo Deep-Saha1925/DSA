9559e259b67a6677d0be9553017fe674630de95b
Deep-Saha1925 <dip23447@gmail.com>
2026-07-08 13:40:03 +0530

    Reach Number

--- ReachNumber_754.java
+++ ReachNumber_754.java
@@ -1,3 +1,18 @@
-public class ReachNumber_754 {
-    
+class Solution {
+
+    public int reachNumber(int target) {
+
+        target = Math.abs(target);
+
+        int moves = 0;
+        int sum = 0;
+
+        while (sum < target || (sum - target) % 2 != 0) {
+
+            moves++;
+            sum += moves;
+        }
+
+        return moves;
+    }
 }
