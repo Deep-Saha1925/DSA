f2c6d783ab76ac282afd427b782e5bfb8bae960d
Deep-Saha1925 <dip23447@gmail.com>
2026-07-10 21:43:23 +0530

    Split LL in parts

--- SplitLL_725.java
+++ SplitLL_725.java
@@ -1,3 +1,44 @@
-public class SplitLL_725 {
-    
+class Solution {
+
+    public ListNode[] splitListToParts(ListNode head, int k) {
+
+        ListNode[] ans = new ListNode[k];
+
+        int length = 0;
+        ListNode curr = head;
+
+        while (curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        int size = length / k;
+        int extra = length % k;
+
+        curr = head;
+
+        for (int i = 0; i < k; i++) {
+
+            if (curr == null) {
+                ans[i] = null;
+                continue;
+            }
+
+            ans[i] = curr;
+
+            int currSize = size + (extra > 0 ? 1 : 0);
+
+            if (extra > 0)
+                extra--;
+
+            for (int j = 1; j < currSize; j++)
+                curr = curr.next;
+
+            ListNode next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+
+        return ans;
+    }
 }
