90faab1dc45fae6f56456ef66fd6e677f6d0ec13
Deep-Saha1925 <dip23447@gmail.com>
2026-07-15 12:59:21 +0530

    Longest word

--- LongWordDict_720.java
+++ LongWordDict_720.java
@@ -1,26 +1,37 @@
 class Solution {
-    public String longestWord(String[] words) {
-        Set<String> s = new HasSet<>();
 
-        for(String w: words){
-            s.add(w);
-        }
+    public String longestWord(String[] words) {
+
+        Set<String> set = new HashSet<>();
+
+        for (String word : words)
+            set.add(word);
 
         String ans = "";
 
-        for(String word: words){
+        for (String word : words) {
+
             boolean valid = true;
 
-            for(int i=1; i<word.length(); i++){
-                if(!set.contains(word.substring(0, i))){
+            for (int i = 1; i < word.length(); i++) {
+
+                if (!set.contains(word.substring(0, i))) {
                     valid = false;
                     break;
                 }
             }
 
-            if(valid){
-                
+            if (valid) {
+
+                if (word.length() > ans.length() ||
+                   (word.length() == ans.length() &&
+                    word.compareTo(ans) < 0)) {
+
+                    ans = word;
+                }
             }
         }
+
+        return ans;
     }
 }
