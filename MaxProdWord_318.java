beb9de493f61b92b529de14253e82ecbd7b2d5ba
Deep-Saha1925 <dip23447@gmail.com>
2026-07-18 21:55:58 +0530

    Maximum product word length

--- MaxProdWord_318.java
+++ MaxProdWord_318.java
@@ -1,3 +1,17 @@
-public class MaxProdWord_318 {
-    
+class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+
+        int[] masks = new int[n];
+
+        for(int i=0; i<n; i++){
+            for(char ch: words[i].toCharArray()){
+                masks[i] |= 1 << (ch - 'a');
+            }
+        }
+
+        int ans = 0;
+
+        
+    }
 }
