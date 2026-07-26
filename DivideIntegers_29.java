1c974fb3ad5b31bdec85f81413ba6e83b51eb04b
Deep-Saha1925 <dip23447@gmail.com>
2026-07-26 10:53:25 +0530

    Divide integers

--- DivideIntegers_29.java
+++ DivideIntegers_29.java
@@ -3,6 +3,19 @@ class Solution {
         if(divisor == Integer.MAX_VALUE && divisor == -1)
             return Integer.MAX_VALUE;
 
-        
+        long dvd = Math.abs((long) dividend);
+        long div = Math.abs((long) divisor);
+
+        int ans = 0;
+
+        while(dvd <= div){
+            long temp = div;
+            int m = 1;
+
+            while(dvd >= (t << 1)){
+                t <<= 1;
+                m <<= 1;
+            }
+        }        
     }
 }
