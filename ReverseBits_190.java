e939acab89298cc62f3403340d5da9c212932b1a
Deep-Saha1925 <dip23447@gmail.com>
2026-07-27 13:04:48 +0530

    Reverse Bits

--- ReverseBits_190.java
+++ ReverseBits_190.java
@@ -1,3 +1,13 @@
-public class ReverseBits_190 {
-    
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for(int i=0; i<32; i++){
+            ans <<= 1;
+
+            ans |= (n & 1);
+        }
+
+        return ans;
+    }
 }
