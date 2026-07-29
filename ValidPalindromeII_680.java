468844dcd4f1df5f08ea22d356856a2df331c3fc
Deep-Saha1925 <dip23447@gmail.com>
2026-07-29 23:56:01 +0530

    Valid Palindome II

--- ValidPalindromeII_680.java
+++ ValidPalindromeII_680.java
@@ -1,5 +1,19 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        return true;
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else {
+                return isPalindrome(s, left + 1, right) ||
+                       isPalindrome(s, left, right - 1);
+            }
+        }
+
+        
     }
 }
