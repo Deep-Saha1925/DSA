d3f673da66d5221395a231a1c5a86490be5f1148
Deep-Saha1925 <dip23447@gmail.com>
2026-07-30 00:15:13 +0530

    Car fleet

--- CarFleet_853.java
+++ CarFleet_853.java
@@ -3,5 +3,12 @@ class Solution {
         int n = position.length;
 
         double[][] cars = new double[n][2];
+
+        for(int i=0; i<n; i++){
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
+        }
+
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
     }
 }
