496b336bc4fd6650a01308d4ca33821864bf473f
Deep-Saha1925 <dip23447@gmail.com>
2026-07-30 00:13:52 +0530

    Car fleet

--- CarFleet_853.java
+++ CarFleet_853.java
@@ -1,5 +1,7 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        
+        int n = position.length;
+
+        double[][] cars = new double[n][2];
     }
 }
