ed881f9a9ddeeb4d69801668fc0362129f1f86bb
Deep-Saha1925 <dip23447@gmail.com>
2026-07-31 21:40:12 +0530

    Merge Two Binary trees

--- MergeBT_617.java
+++ MergeBT_617.java
@@ -16,5 +16,7 @@
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
         if(root1 == null)   return root2;
+
+        if(root2 == null)   return root1;
     }
 }
