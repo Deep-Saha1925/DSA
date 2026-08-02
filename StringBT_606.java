24bcd169000be5bc099fc927496f61ee0d34d04d
Deep-Saha1925 <dip23447@gmail.com>
2026-08-02 21:19:24 +0530

    BST 2 String

--- StringBT_606.java
+++ StringBT_606.java
@@ -16,5 +16,11 @@
 class Solution {
     public String tree2str(TreeNode root) {
         if(root == null) return "";
+
+        String ans = String.valueOf(root.val);
+
+        if(root.left != null || root.right != null){
+            ans += "(" + tree2str(root.left) + ")";
+        }
     }
 }
