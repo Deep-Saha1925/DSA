6b639b22f2617b83740ae6fb140724f95bc18ee5
Deep-Saha1925 <dip23447@gmail.com>
2026-08-02 21:19:58 +0530

    tree2str

--- StringBT_606.java
+++ StringBT_606.java
@@ -1,26 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public String tree2str(TreeNode root) {
-        if(root == null) return "";
+
+        if (root == null)
+            return "";
 
         String ans = String.valueOf(root.val);
 
-        if(root.left != null || root.right != null){
+        if (root.left != null || root.right != null) {
             ans += "(" + tree2str(root.left) + ")";
         }
+
+        if (root.right != null) {
+            ans += "(" + tree2str(root.right) + ")";
+        }
+
+        return ans;
     }
 }
