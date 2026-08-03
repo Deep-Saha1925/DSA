6e1a9080198fd488e7b9daffa91970b2e4c9f787
Deep-Saha1925 <dip23447@gmail.com>
2026-08-03 20:43:01 +0530

    Binary Tree Pruning

--- BTPruning_814.java
+++ BTPruning_814.java
@@ -20,5 +20,10 @@ class Solution {
         root.left = pruneTree(root.left);
         root.right = pruneTree(root.right);
 
+        if(root.val == 0 && root.left == null && root.right == null){
+            return null;
+        }
+
+        return root;
     }
 }
