58e26ddd26e709d1ef5a4d82c2a981b173a257d1
Deep-Saha1925 <dip23447@gmail.com>
2026-08-05 22:00:59 +0530

    Add row to tree

--- AddRowTree_623.java
+++ AddRowTree_623.java
@@ -1,20 +1,38 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public TreeNode addOneRow(TreeNode root, int val, int depth) {
+
+        if (depth == 1) {
+            TreeNode newRoot = new TreeNode(val);
+            newRoot.left = root;
+            return newRoot;
+        }
+
+        dfs(root, val, 1, depth);
+
         return root;
+    }
+
+    private void dfs(TreeNode root, int val, int level, int depth) {
+
+        if (root == null)
+            return;
+
+        if (level == depth - 1) {
+
+            TreeNode oldLeft = root.left;
+            TreeNode oldRight = root.right;
+
+            root.left = new TreeNode(val);
+            root.right = new TreeNode(val);
+
+            root.left.left = oldLeft;
+            root.right.right = oldRight;
+
+            return;
+        }
+
+        dfs(root.left, val, level + 1, depth);
+        dfs(root.right, val, level + 1, depth);
     }
 }
