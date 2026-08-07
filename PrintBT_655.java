037465eeb1c79dab653a9889ef14965669bb41a1
Deep-Saha1925 <dip23447@gmail.com>
2026-08-07 22:00:37 +0530

    Print Binary tree

--- PrintBT_655.java
+++ PrintBT_655.java
@@ -1,3 +1,35 @@
-public class PrintBT_655 {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<String>> printTree(TreeNode root) {
+         int h = height(root);
+
+        int rows = h + 1;
+        int cols = (1 << (h + 1)) - 1;
+
+        List<List<String>> ans = new ArrayList<>();
+
+        for (int i = 0; i < rows; i++) {
+
+            List<String> row = new ArrayList<>();
+
+            for (int j = 0; j < cols; j++)
+                row.add("");
+
+            ans.add(row);
+        }
+    }
 }
