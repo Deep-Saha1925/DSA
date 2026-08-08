37158c4fa0ed2d807dd48149528c5dbacac8140c
Deep-Saha1925 <dip23447@gmail.com>
2026-08-08 22:04:57 +0530

    updated code

--- py/DoublyLL.py
+++ py/DoublyLL.py
@@ -3,11 +3,11 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
- 
+
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
- 
+
     def insert_at_end(self, data):
         newNode = Node(data)
         if self.head == None:
