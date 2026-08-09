9d23ca16b1bbdf495529be9b18b396695601bded
Deep-Saha1925 <dip23447@gmail.com>
2026-08-09 19:43:20 +0530

    Base 7

--- Base7_504.java
+++ Base7_504.java
@@ -1,5 +1,21 @@
 class Solution {
     public String convertToBase7(int num) {
-        return num;
+        if(num == 0) return "0";
+
+        boolean neg = num < 0;
+
+        long n = Math.abs((long) num);
+
+        StringBuilder sb = new StringBuilder();
+
+        while(n > 0){
+            sb.append(n % 7);
+            n /= 7;
+        }
+
+        if(neg)
+            sb.append('-');
+
+        return sb.reverse().toString();
     }
 }
