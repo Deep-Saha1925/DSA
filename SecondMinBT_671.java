c1a0db27cbc946d44a7b2a90889c1f95e076019b
Deep-Saha1925 <dip23447@gmail.com>
2026-08-10 17:11:14 +0530

    Second min in BT

--- SecondMinBT_671.java
+++ SecondMinBT_671.java
@@ -1,20 +1,9 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
+    long ans = Long.MAX_VALUE;
+
     public int findSecondMinimumValue(TreeNode root) {
+
         int min = root.val;
 
         dfs(root, min);
@@ -22,12 +11,16 @@ class Solution {
         return ans == Long.MAX_VALUE ? -1 : (int) ans;
     }
 
-    private void dfs(TreeNode root, int min){
-        if(root == null) return;
+    private void dfs(TreeNode node, int min) {
 
-        if(root.val > min)
-            ans = Math.min(ans, root.val);
+        if (node == null)
+            return;
 
-            
+        if (node.val > min) {
+            ans = Math.min(ans, node.val);
+        }
+
+        dfs(node.left, min);
+        dfs(node.right, min);
     }
 }
