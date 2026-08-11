3c7ab4de63d35a2f376898d96dfd0f507387795c
Deep-Saha1925 <dip23447@gmail.com>
2026-08-11 15:32:17 +0530

    Count elements

---
+++ py/Count.py
@@ -0,0 +1,9 @@
+# Count elements are gerater than avg
+l = [1, 2, 3, 4, 5]
+avg = sum(l) / len(l)
+count = 0
+for i in l:
+    if i > avg:
+        count += 1
+
+print("Count of elements greater than average:", count)
