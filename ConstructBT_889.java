e30c744fe29b72a09297d367bfa984d65d14b5cf
Deep-Saha1925 <dip23447@gmail.com>
2026-08-12 20:52:12 +0530

    Construct BT

--- ConstructBT_889.java
+++ ConstructBT_889.java
@@ -1,24 +1,11 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
 
     private Map<Integer, Integer> map = new HashMap<>();
 
     public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
-        for(int i=0; i<postorder.length; i++){
+
+        // Store postorder indices
+        for (int i = 0; i < postorder.length; i++) {
             map.put(postorder[i], i);
         }
 
@@ -29,9 +16,49 @@ class Solution {
     }
 
     private TreeNode build(
-        int[] preorder, int preStr, int preEnd,
-        int[] postorder, int postStr, int postEnd
-    ) {
-        
+            int[] preorder, int preStart, int preEnd,
+            int[] postorder, int postStart, int postEnd) {
+
+        if (preStart > preEnd)
+            return null;
+
+        TreeNode root = new TreeNode(preorder[preStart]);
+
+        // Only one node
+        if (preStart == preEnd)
+            return root;
+
+        // Next preorder element is left subtree root
+        int leftRoot = preorder[preStart + 1];
+
+        // Find it in postorder
+        int index = map.get(leftRoot);
+
+        // Size of left subtree
+        int leftSize = index - postStart + 1;
+
+        // Build left subtree
+        root.left = build(
+            preorder,
+            preStart + 1,
+            preStart + leftSize,
+
+            postorder,
+            postStart,
+            index
+        );
+
+        // Build right subtree
+        root.right = build(
+            preorder,
+            preStart + leftSize + 1,
+            preEnd,
+
+            postorder,
+            index + 1,
+            postEnd - 1
+        );
+
+        return root;
     }
 }
