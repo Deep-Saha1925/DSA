b2cd2caf34fa35789f5ad0c394640057e266dd76
Deep-Saha1925 <dip23447@gmail.com>
2026-08-13 21:05:48 +0530

    All BTs.

--- AllfullBt_894.java
+++ AllfullBt_894.java
@@ -4,5 +4,17 @@ class Solution {
 
         List<TreeNode> result = new ArrayList<>();
 
+        // Full binary tree cannot have even nodes
+        if (n % 2 == 0)
+            return result;
+
+        // One node
+        if (n == 1) {
+            result.add(new TreeNode(0));
+            return result;
+        }
+
+        // Try every possible left subtree size
         
+    }
 }
