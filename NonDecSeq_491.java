289d550f51c78abf4fb7e0f0175f72da9ed7220a
Deep-Saha1925 <dip23447@gmail.com>
2026-08-17 14:39:12 +0530

    Non decreasing subseq

--- NonDecSeq_491.java
+++ NonDecSeq_491.java
@@ -4,4 +4,35 @@ class Solution {
         backtrack(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
+
+    private void backtrack(int[] nums, int start,
+                           List<Integer> list,
+                           List<List<Integer>> ans) {
+
+        if (list.size() >= 2) {
+            ans.add(new ArrayList<>(list));
+        }
+
+        HashSet<Integer> used = new HashSet<>();
+
+        for (int i = start; i < nums.length; i++) {
+
+            // Avoid duplicates at this recursion level
+            if (used.contains(nums[i])) {
+                continue;
+            }
+
+            // Must be non-decreasing
+            if (!list.isEmpty() && nums[i] < list.get(list.size() - 1)) {
+                continue;
+            }
+
+            used.add(nums[i]);
+            list.add(nums[i]);
+
+            backtrack(nums, i + 1, list, ans);
+
+            list.remove(list.size() - 1);
+        }
+    }
 }
