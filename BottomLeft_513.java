5d43ecb7d0c366e4496e4e233fad7ac248e4ac8a
Deep-Saha1925 <dip23447@gmail.com>
2026-08-19 11:06:46 +0530

    Bottom left tree value

--- BottomLeft_513.java
+++ BottomLeft_513.java
@@ -1,28 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+
     public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
         int ans = root.val;
 
-        while(!queue.isEmpty()){
+        while (!q.isEmpty()) {
+
             int size = q.size();
 
-            for(int i=0; i<size; i++){
+            for (int i = 0; i < size; i++) {
+
                 TreeNode node = q.poll();
 
                 // First node of this level
@@ -39,5 +29,7 @@ class Solution {
                 }
             }
         }
+
+        return ans;
     }
 }
