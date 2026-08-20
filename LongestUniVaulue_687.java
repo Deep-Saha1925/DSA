944590dcd3d62f6536a8098c5f4bcae458202222
Deep-Saha1925 <dip23447@gmail.com>
2026-08-20 15:20:21 +0530

    Longest univalue path

--- LongestUniVaulue_687.java
+++ LongestUniVaulue_687.java
@@ -1,3 +1,36 @@
-public class LongestUniVaulue_687 {
-    
+class Solution {
+
+    int ans = 0;
+
+    public int longestUnivaluePath(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+
+        if (node == null) {
+            return 0;
+        }
+
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+
+        int leftPath = 0;
+        int rightPath = 0;
+
+        if (node.left != null && node.left.val == node.val) {
+            leftPath = left + 1;
+        }
+
+        if (node.right != null && node.right.val == node.val) {
+            rightPath = right + 1;
+        }
+
+        // Path passing through current node
+        ans = Math.max(ans, leftPath + rightPath);
+
+        // Return only one direction to parent
+        return Math.max(leftPath, rightPath);
+    }
 }
