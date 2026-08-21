dedb866ae1cd2e84bf6eeeeb4c6cd11ee7e3b755
Deep-Saha1925 <dip23447@gmail.com>
2026-08-21 10:01:32 +0530

    Sum root to leaf

--- SumRootLeaf_1022.java
+++ SumRootLeaf_1022.java
@@ -17,4 +17,16 @@ class Solution {
     public int sumRootToLeaf(TreeNode root) {
         return dfs(root, 0);
     }
+
+    private int dfs(TreeNode node, int val){
+        if(node == null)  return 0;
+
+        val = val * 2 + node.val;
+
+        if(node.left == null && node.right == null){
+            return val;
+        }
+
+        
+    }
 }
