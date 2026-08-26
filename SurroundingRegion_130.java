1acc7d5a824daa5379f8a261d2c33a976bd9863d
Deep-Saha1925 <dip23447@gmail.com>
2026-08-26 23:29:58 +0530

    Surrounding Region

--- SurroundingRegion_130.java
+++ SurroundingRegion_130.java
@@ -1,5 +1,18 @@
 class Solution {
     public void solve(char[][] board) {
-        
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // First column and last column
+        for (int i = 0; i < rows; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, cols - 1);
+        }
+
+        // First row and last row
+        for (int j = 0; j < cols; j++) {
+            dfs(board, 0, j);
+            dfs(board, rows - 1, j);
+        }
     }
 }
