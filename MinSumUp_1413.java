397e13d4510b37f394415fce67c2d72ab297039f
Deep-Saha1925 <dip23447@gmail.com>
2026-08-27 17:10:42 +0530

    Minimum sum up

--- MinSumUp_1413.java
+++ MinSumUp_1413.java
@@ -2,6 +2,12 @@ class Solution {
     public int minStartValue(int[] nums) {
         int sum = 0, minSum = 0;
 
+        for(int n: nums){
+            sum += n;
+
+            minSum = Math.min(minSum, sum);
+        }
+
         return minSum;
     }
 }
