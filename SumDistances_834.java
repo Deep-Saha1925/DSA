301842ee21bf91e493015cbefef1aa4d4cf2d367
Deep-Saha1925 <dip23447@gmail.com>
2026-09-01 22:08:28 +0530

    Sum distance

--- SumDistances_834.java
+++ SumDistances_834.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+
 class Solution {
+
+    List<Integer>[] tree;
+    int[] ans, count;
+    int n;
+
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        int[] ans = new int[n];
+        this.n = n;
+        tree = new ArrayList[n];
+
+        for(int i=0; i<n; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        
 
         return ans;
     }
