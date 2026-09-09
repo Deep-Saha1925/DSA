ecc5bb92e157855197a73d7e59d6e2027b3aacec
Deep-Saha1925 <dip23447@gmail.com>
2026-09-09 23:07:03 +0530

    Longest Absoulte path

--- LongestAbsPath_388.java
+++ LongestAbsPath_388.java
@@ -4,7 +4,15 @@ class Solution {
 
         int[] pathLen = new int[lines.length+1];
         int max = 0;
-    
-        
+
+        int depth = 0;
+        while(line.chatAt(depth) == "\t") {
+            depth++;
+        }
+
+        //removing tabs
+        String name = line.substring(depth);
+
+        pathLen[depth] = pathLen[depth - 1] + name.length() + 1;
     }
 }
